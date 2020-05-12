b6023cbfa297e815186f3de1f95b657c3f565e47
tomjamescn <tomjamescn@gmail.com>
2020-05-12 21:41:55 +0800

    bugfix

--- registry/registry.go
+++ registry/registry.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 )
 
-var Center map[string]http.Handler
+var Center = map[string]http.Handler{}
 
 func Register(serviceName string, h http.Handler) {
 	Center[serviceName] = h
