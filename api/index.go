ac9cd4d87e4d6700eb4981f4f5bb69cf237424bc
tomjamescn <tomjamescn@gmail.com>
2020-05-12 22:43:21 +0800

    debug

--- api/index.go
+++ api/index.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	_ "github.com/tomjamescn/siri-whatismyip"
@@ -12,6 +13,8 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	var (
 		service = r.URL.Query().Get("service")
 	)
+
+	log.Printf("header: %#v remoteAddr: %s", r.Header, r.RemoteAddr)
 
 	if h, ok := registry.Center[service]; ok {
 		h.ServeHTTP(w, r)
